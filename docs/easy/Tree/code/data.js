aefc34dd73728f3e90bfda4bd14d1a4f806c38fa
johnzhu <1042532367@qq.com>
2021-11-16 18:50:31 +0800

    update

---
+++ docs/easy/Tree/code/data.js
@@ -0,0 +1,18 @@
+var tree = {
+    val: '1',
+    left: {
+        val: '2',
+        left: {
+            val: '4',
+            left: null,
+            right: null
+        },
+        right: null
+    },
+    right: {
+        val: '3',
+        left: null,
+        right: null
+    }
+}
+module.exports.tree = tree
--- docs/easy/Tree/code/tree2str.js
+++ docs/easy/Tree/code/tree2str.js
@@ -1,3 +1,21 @@
-var tree2str = function (root) {
+var { tree } = require('./data.js')
 
+var tree2str = function (root) {
+    let dfs = (n) => {
+        if (!n) return ''
+        let res = ''
+        if (!n.left && !n.right) {
+            return res + `${n.val}`
+        }
+        res = `${n.val}`
+        if (n.left) res += `(${dfs(n.left)})`
+        if (!n.left && n.right) res += '()'
+        if (n.right) res += `(${dfs(n.right)})`
+        return res
+    }
+    return dfs(root)
 };
+
+
+var res = tree2str(tree)
+console.log(res)
